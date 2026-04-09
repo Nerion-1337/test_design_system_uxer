19c5ae80b11ffa55b24d38e9213e2ab166bf8166
Nerion <nerion.1337.dev@gmail.com>
2026-04-09 12:55:01 +0000

    reel mixin

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,48 @@ import {
 /// UI
 import Button from "#components/ui/Button";
 import ButtonGroup from "#components/ui/ButtonGroup";
+import { useMemo, useState } from "react";
 //
 //
 //
 //
 //
 function App() {
+const [size, setSize] = useState("M");
+const [variant, setVariant] = useState("standard");
+const [color, setColor] = useState("destructive");
+const [disabled, setDisabled] = useState(false);
+
+const test = useMemo(() => {
+  return ( <Button
+        variant={variant}
+        size={size}
+        color={color}
+        iconLeft={<PiCheckBold />}
+        children="Valider"
+        disabled={disabled}
+      />)
+}, [size, variant, color]);
+
+const fonciton = (param) => {
+  setSize(param);
+}
+
+const fonciton2 = (param) => {
+  setVariant(param);
+}
+
+const fonciton3 = (param) => {
+  if(color === "destructive") {
+    setDisabled(true);
+  } else {
+    setColor(param);
+  }
+}
+
   return (
     <section>
+      {test}
       <Button
         variant="standard"
         size="M"
@@ -23,6 +57,7 @@ function App() {
         iconLeft={<PiCheckBold />}
         children="Valider"
         disabled={false}
+        onClick={() => fonciton("L")}
       />
       <Button
         variant="standard"
@@ -39,7 +74,8 @@ function App() {
           size="M"
           color="destructive"
           children="Supprimer"
-          disabled={false}
+          disabled={disabled}
+          onClick={() => fonciton3("primary")}
         />
         <Button
           variant="icon"
@@ -48,6 +84,7 @@ function App() {
           children={<PiGearSix />}
           disabled={false}
           ariaLabel="Settings"
+          onClick={() => fonciton2("icon")}
         />
       </ButtonGroup>
       <Button
